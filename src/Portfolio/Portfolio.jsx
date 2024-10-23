87ef3f1b1aa4f35ff5183eadc3b83395867de073
Gagandeep075 <134015361+Gagandeep075@users.noreply.github.com>
2024-10-22 21:42:20 -0400

    Update Portfolio.jsx

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -7,14 +7,40 @@ const items = [
         id: 1,
         title: "Netflix Clone",
         img: "https://upload.wikimedia.org/wikipedia/en/3/3c/Netflix_UI_for_Web.png",
-        desc:" A <strong>Netflix clone</strong> built with <strong>HTML, CSS, JavaScript, React, Redux, MongoDB, and Tailwind CSS</strong> offers a fully functional streaming platform. It features a sleek, responsive UI with movie categories, thumbnails, and search functionality. <strong>Redux</strong> manages state, while <strong>MongoDB</strong> handles user authentication and login.The app makes secure API calls to retrieve movie data, delivering an experience similar to Netflix.",
+        desc: `
+        <ul>
+            <li>A <strong>Netflix clone</strong> built with the following technologies:</li>
+            <ul>
+                <li><strong>HTML, CSS, JavaScript, React, Redux, MongoDB, Tailwind CSS</strong></li>
+            </ul>
+            <li>Features:</li>
+            <ul>
+                <li><strong>Sleek, responsive UI</strong> with movie categories, thumbnails, and search functionality.</li>
+                <li><strong>Redux</strong> for state management.</li>
+                <li><strong>MongoDB</strong> handles user authentication and login.</li>
+                <li><strong>Secure API calls</strong> to retrieve movie data, mimicking Netflix's experience.</li>
+            </ul>
+        </ul>`,
         link: "https://netlfix-clone-gagan-2.netlify.app"
     },
     {
         id: 2,
         title: "Apple Clone",
         img: "https://patentlyapple.typepad.com/.a/6a0120a5580826970c02c8d39dd4a0200b-800wi",
-        desc: `The <strong>Apple website clone</strong> is a front-end project built using <strong>HTML, CSS, JavaScript, React, Tailwind CSS, and GSAP</strong>. It includes three pages: <strong>iPhone</strong>, <strong>iPad</strong>, and <strong>Apple Vision Pro</strong>, styled to replicate Apple's sleek design. React powers dynamic rendering and smooth navigation, while Tailwind ensures responsiveness. GSAP adds smooth, scroll-triggered animations for an interactive user experience. Please Click on <strong>Learn More</strong> button to see other pages`,
+        desc: `
+        <ul>
+            <li>An <strong>Apple website clone</strong> created using:</li>
+            <ul>
+                <li><strong>HTML, CSS, JavaScript, React, Tailwind CSS, GSAP</strong></li>
+            </ul>
+            <li>Key components:</li>
+            <ul>
+                <li>Three pages: <strong>iPhone, iPad, and Apple Vision Pro</strong>.</li>
+                <li><strong>React</strong> powers dynamic rendering and smooth navigation.</li>
+                <li><strong>Tailwind CSS</strong> ensures responsiveness and style consistency.</li>
+                <li><strong>GSAP</strong> adds smooth, scroll-triggered animations for an interactive experience.</li>
+            </ul>
+        </ul>`,
         link: "https://apple-clone-gagan.netlify.app"
         
     }
@@ -23,10 +49,8 @@ const items = [
 const Single = ({item}) => {
     const ref = useRef();
 
-    
     const {scrollYProgress} = useScroll({
         target: ref,
-        
     });
 
     const Y = useTransform(scrollYProgress, [0,1], [-400, 400]);
@@ -35,13 +59,13 @@ const Single = ({item}) => {
         <div className="container">
             <div className="wrapper">
                 <div className="imageContainer">
-            <img src={item.img} ref={ref} />
-            </div>
-            <motion.div style={{y: Y}} className="textContainer">
-                <h2>{item.title}</h2>
-                <p dangerouslySetInnerHTML={{ __html: item.desc }}></p>
-                <button><a href={item.link} target="_blank">See Demo</a></button>
-            </motion.div>
+                    <img src={item.img} ref={ref} />
+                </div>
+                <motion.div style={{y: Y}} className="textContainer">
+                    <h2>{item.title}</h2>
+                    <p dangerouslySetInnerHTML={{ __html: item.desc }}></p>
+                    <button><a href={item.link} target="_blank">See Demo</a></button>
+                </motion.div>
             </div>
         </div>
     </section>);
@@ -61,17 +85,17 @@ const Portfolio = () => {
         damping: 30,
     })
 
-  return (
-    <div className="portfolio" ref={ref}>
-        <div className="progress">
-            <h1>Projects</h1>
-            <motion.div style={{scaleX}} className="progressBar"></motion.div>
+    return (
+        <div className="portfolio" ref={ref}>
+            <div className="progress">
+                <h1>Projects</h1>
+                <motion.div style={{scaleX}} className="progressBar"></motion.div>
+            </div>
+            {items.map((item) => (
+                <Single item={item} key={item.id} />
+            ))}
         </div>
-        {items.map((item) => (
-            <Single item={item} key={item.id} />
-        ))}
-    </div>
-  )
+    )
 }
 
 export default Portfolio
